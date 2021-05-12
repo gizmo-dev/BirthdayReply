0efacd06a15f87d1117339dc5411779784ed42da
gizmo-dev <gizmoflash0@gmail.com>
2021-05-13 04:11:41 +0530

    main

---
+++ README.md
@@ -0,0 +1,36 @@
+# Birthday Wish Reply
+> Instantly Reply To Birthday Wishes
+
+Instantly reply to the birthday Wishes you recieve on whatsapp and instagram using this simple bot!
+PS. I Don't recommend doing this as it will piss of the few friends you have! 😂
+![](example.png)
+
+## Installation
+
+```sh
+npm install @open-wa/wa-automate
+```
+```sh
+npm install @androz2091/insta.js
+```
+
+## Setup
+> INSTAGRAM
+Replace
+```js
+client.login('username', 'pass');
+```
+with your Instagram Username and Password
+
+>WHATSAPP
+SCAN THE QR CODE GENERATED IN YOUR TERMINAL ONCE YOU RUN THE CODE WITH YOUR WHATSAPP APPLICATION AS A WHATSAPP WEB LOGIN
+
+>START 
+```sh
+node main.js
+```
+
+## Atrributions
+
+> Thanks To [@open-wa](https://github.com/open-wa) for the whatsapp package! [wa-automate-nodejs](https://github.com/open-wa/wa-automate-nodejs)
+> tThanks To [@androz2091](https://github.com/Androz2091) For the instagram package! [insta.js](https://github.com/Androz2091/insta.js)
---
+++ main.js
@@ -0,0 +1,59 @@
+// Thankyou messages
+const msgs = [
+    `Thanks♥`,
+    `Tysm♥`,
+    `Thank You♥`,
+    `Thank You💖`,
+]
+var options = Math.floor(Math.random() * msgs.length);
+var thanks = msgs[options]
+//whatsapp settings
+const wa = require('@open-wa/wa-automate');
+wa.create({
+    sessionId: "Birthday",
+    authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+    blockCrashLogs: true,
+    disableSpins: true,
+    headless: true,
+    hostNotificationLang: 'PT_BR',
+    logConsole: false,
+    popup: true,
+    qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+  }).then(client => start(client));
+
+  function start(client) {
+    client.onMessage(async message => {
+      if(message.body.toLowerCase().includes('birthday')) return client.sendText(message.from, thanks);
+      if(message.body.toLowerCase().includes('bday')) return client.sendText(message.from,thanks);
+      if(message.body.toLowerCase().includes('happy birthday')) return client.sendText(message.from,thanks);
+      if(message.body.toLowerCase().includes('born day')) return client.sendText(message.from, thanks);
+      if(message.body.toLowerCase().includes('bornday')) return client.sendText(message.from, thanks);
+      if(message.body.toLowerCase().includes('birthday day')) return client.sendText(message.from, thanks);
+      
+    });
+}
+
+//instagram settings
+const Insta = require('@androz2091/insta.js');
+
+const client = new Insta.Client();
+
+
+client.on('connected', () => {
+    console.log(`Logged in as ${client.user.username}`);
+});
+client.on('messageCreate', (message) => {
+    if (message.author.id === client.user.id) return
+
+    message.markSeen();
+
+    if(message.content.toLowerCase().includes('happy birthday')) return message.chat.sendMessage(thanks);
+    if(message.content.toLowerCase().includes('birthday')) return message.chat.sendMessage(thanks);
+    if(message.content.toLowerCase().includes('bday')) return message.chat.sendMessage(thanks);
+    if(message.content.toLowerCase().includes('bornday')) return message.chat.sendMessage(thanks);
+    if(message.content.toLowerCase().includes('born day')) return message.chat.sendMessage(thanks);      
+    if(message.content.toLowerCase().includes('birth day')) return message.chat.sendMessage(thanks);   
+    
+});
+
+client.login('username', 'pass'); // enter the account info for which you wanna use the bot
